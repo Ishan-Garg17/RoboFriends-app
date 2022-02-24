2a183d31ffd5e428cf3c0e325956995a54b20320
Ishan-Garg17 <ishangarg17@gmail.com>
2022-02-24 19:36:23 +0530

    Modifying app

---
+++ src/CardList.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import {robots} from './robots'
+import Card from "./Card";
+
+const CardList = ({robots,key})=>{
+    const cardArray = robots.map((user,i)=>{
+        return(
+            <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+            )
+    })
+    
+    return(
+        <div>
+            {cardArray}
+        </div>
+    )
+}
+
+
+// <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+//       <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+//       <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+
+
+export default CardList
