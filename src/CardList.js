346f50baed8d5f6a79fbd03525a905afbd7720df
Ishan-Garg17 <ishangarg17@gmail.com>
2022-02-24 20:12:38 +0530

    edits

--- src/CardList.js
+++ src/CardList.js
@@ -1,8 +1,7 @@
 import React from "react";
-// import {robots} from './robots'
 import Card from "./Card";
 
-const CardList = ({robots,key})=>{
+const CardList = ({robots})=>{
     const cardArray = robots.map((user,i)=>{
         return(
             <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
