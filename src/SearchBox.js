00b8c3cb7a0419af882b96a12c524b08c1abae7a
Ishan-Garg17 <ishangarg17@gmail.com>
2022-02-25 22:12:25 +0530

    Completed

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchBox = ({searchChange})=>{
     return(
-            <input className="tc"
+            <input className="pa3 tc ba b--green bg-lightest-blue"
             type="search" 
             placeholder="Search Robots"
             onChange = {searchChange} 
